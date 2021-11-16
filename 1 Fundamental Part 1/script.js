5a64a3fe8c22450c29ea86b01becd24aad1a7ead
ashumsd7 <aat8001@gmail.com>
2021-11-16 21:36:29 +0530

    varaible naming and convenstions

--- 1 Fundamental Part 1/script.js
+++ 1 Fundamental Part 1/script.js
@@ -1,2 +1,20 @@
 let js= "amazing";
 if(js==='amazing') alert('Javascript is easy')
+
+
+
+let firstName= "Lovely"
+//varibale names can't start with numbers
+// it can start with $ or _  or letters \ no numbers are allowed
+let 4var= 'ashu'
+
+
+//some reserved keywords are also not allowed
+let new = 34;
+
+
+let $function = 34;
+
+
+let PI= 3.14;
+
