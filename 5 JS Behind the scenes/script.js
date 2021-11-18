1a38e97799dc9b3e4d0b3efa730fe9c9800baee4
ashumsd7 <aat8001@gmail.com>
2021-11-18 20:53:48 +0530

    Behind the scene : some parts done

--- 5 JS Behind the scenes/script.js
+++ 5 JS Behind the scenes/script.js
@@ -1,1 +1,131 @@
 'use strict';
+
+// function calcAge(birthYear){
+//     const age= 2037- birthYear;
+//     console.log(firstName);
+//     function printAge(){
+//         const output= `You are ${firstName} and Your age is  ${age} born in ${birthYear}`
+//         console.log(output);
+//     }
+//     printAge()
+//     return age;
+// }
+
+
+// const firstName= 'Ashu'
+// calcAge(1991)
+
+// {
+
+//     function printName(){
+//         console.log("my name is printed");
+//     }
+
+    
+// }
+
+// printName()
+
+
+//HOISTING
+
+// let a;
+// console.log(a);
+// a=10
+
+// //>> Starting of TDZ for let and const 
+// console.log(me);  
+// //undefined
+// console.log(job);  
+// //Cannot access 'job' before initialization
+// console.log(birthYear); 
+// //Cannot access 'birthYear' before initialization
+
+// var me = "ashu";
+// //>> Ending of TDZ for let and const 
+// let job =" coder"
+// const birthYear= "1999"
+
+
+
+// console.log(addDecl(1,2));
+// console.log(addExpr(1,3));
+// console.log(addArrpow);
+
+// function addDecl(a,b){
+//     return a+b;
+// }
+
+// var  addExpr= function(a,b){
+//     return a+b;
+// }
+
+// var  addArrpow= (a,b) => a+b;
+
+
+// var a;
+// let b=2;
+// const c=3;
+
+// console.log(window);
+// console.log(a=== window.x);
+// console.log(b=== window.b);
+// console.log(c=== window.c);
+
+// This keyword
+
+
+// function checkThis(){
+//     console.log(this);
+// }
+
+
+// checkThis();
+
+
+// function arrowThis(){
+//     console.log(this);
+// }
+
+// arrowThis()
+
+
+// How this keyword work : Practise
+
+// console.log(this);
+
+
+
+// const obj={
+//     name: 'ashu',
+
+// }
+
+// obj.myMath=window.Math.random
+
+// console.log(obj.myMath());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
