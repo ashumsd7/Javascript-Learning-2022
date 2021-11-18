4d3919949a790fd0b32766668baa18333cf45c95
ashumsd7 <aat8001@gmail.com>
2021-11-18 15:57:28 +0530

    modal project finished

--- 4 Modal Project/script.js
+++ 4 Modal Project/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const addhideClass = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const removeHideCLass = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', removeHideCLass);
+
+btnCloseModal.addEventListener('click', addhideClass);
+overlay.addEventListener('click', addhideClass);
+
+document.addEventListener('keypress',(e)=>{
+   if(e.key=='Enter'){
+    addhideClass()
+       
+       
+   }
+
+})
