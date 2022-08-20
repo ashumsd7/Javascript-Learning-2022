9fb3016d069f3a27abcf94d41aef81d40023b763
aathym <aat8001@gmail.com>
2022-08-20 15:03:28 +0530

    read : Inversion of control

--- Namaste JS/Callback.js
+++ Namaste JS/Callback.js
@@ -4,11 +4,16 @@ const cart = ["shoes", "pants", "chair"];
 // Call back hell example
 api.createOrder(cart, function () {
   console.log("Creating order");
-  api.proceedToPayment(function () {
-    console.log("Doing Payment");
-    api.showOrderSummary(function () {
-        console.log("Printing  Summary");
-      api.updateProfile();
-    });
-  });
+  api.proceedToPayment();
 });
+
+
+
+
+// inversion of control
+
+// giving function control to other funtion
+
+
+
+
